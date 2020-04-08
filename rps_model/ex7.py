e62b556c5d30f3e47490420e63eb6c0a7d55f455
Bartek Stokłosa <b_stoklosa@o2.pl>
2020-04-08 12:01:17 +0200

    decision tree graphviz

--- rps_model/ex7.py
+++ rps_model/ex7.py
@@ -5,23 +5,19 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import precision_score, accuracy_score, recall_score, f1_score, roc_curve, roc_auc_score
 from sklearn.model_selection import train_test_split, KFold
 
-pd.options.display.max_columns = 6
+pd.options.display.max_columns = 8
 
 df = pd.read_csv('titanic.csv')
 
 df['male'] = df['Sex'] == 'male'
+df['AgeSurv'] = (df['Age'].values <= 30) & (df['Survived'].values == 1)
+df['SexSurv'] = (df['male'].values == True) & (df['Survived'] == 1)
 X = df[['Pclass', 'male', 'Age', 'Siblings/Spouses', 'Parents/Children', 'Fare']].values
-X_age = df[['Age']].values
+X_age = df[['Age']].values > 30
 y = df['Survived'].values
+print((df['AgeSurv'].values).sum())
+print(X_age.sum())
+print((df['SexSurv'].values).sum())
 
-model = LogisticRegression()
-model.fit(X_age, y)
-# print(model.predict(X_age))
-# print(X_age.shape)
-print((model.predict_proba(X_age)[:, 1] > 0.75).sum())
-print(np.mean(X_age))
-print(np.max(X_age))
-print(np.min(X_age))
-print(np.median(X_age))
-plt.scatter(X_age, X_age[::-1], c=y)
+plt.scatter(df['Age'], df['Fare'], c=df['Survived'])
 plt.show()
---
+++ rps_model/ex8.py
@@ -0,0 +1,33 @@
+import graphviz as graphviz
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import precision_score, accuracy_score, recall_score, f1_score, roc_curve, roc_auc_score
+from sklearn.model_selection import train_test_split, KFold
+from sklearn.tree import DecisionTreeClassifier, export_graphviz
+
+
+pd.options.display.max_columns = 8
+
+df = pd.read_csv('titanic.csv')
+df['male'] = df['Sex'] == 'male'
+X = df[['Pclass', 'male', 'Age', 'Siblings/Spouses', 'Parents/Children', 'Fare']].values
+y = df['Survived'].values
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=22)
+model = DecisionTreeClassifier(criterion='entropy')  # default 'gini'
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+print(model.predict(X[:15]))  # predicts for first 15 datasets
+print('Model score: ',  model.score(X_test, y_test))
+print('Accuracy: ', accuracy_score(y_test, y_pred))
+print('Recall: ', recall_score(y_test, y_pred))
+print('Precision: ', precision_score(y_test, y_pred))
+print('F1 score: ', f1_score(y_test, y_pred))
+
+# TODO KFold cross validation for criterion 'gini' and 'entropy'
+
+dot_file = export_graphviz(model)
+graph = graphviz.Source(dot_file)
+graph.render(filename='tree', format='png', cleanup=True)
