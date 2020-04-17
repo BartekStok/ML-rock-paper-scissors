a8d5ff0c83f8d20daf83b29853c87b1856715573
Bartek Stokłosa <b_stoklosa@o2.pl>
2020-04-17 19:42:14 +0200

    plotting sample images

--- rps_model/model_train.py
+++ rps_model/model_train.py
@@ -27,40 +27,66 @@ img_array = np.array([np.asarray(i) for i in img_values])
 X = img_array.reshape(img_values.size, -1)
 y = df['label'].values
 
+# Plotting sample pictures
+labels = np.unique(y)
+fig, axes = plt.subplots(1, labels.size)
+fig.tight_layout()
+fig.suptitle('Random pictures example', fontsize=25)
+for ax, label in zip(axes, labels):
+    idx = np.random.choice(np.argwhere(y == label).reshape(-1))
+    ax.imshow(img_values[idx])
+    ax.set_title(y[idx], fontsize=18)
+    ax.axis('off')
+fig.show()
+
+# Plotting number of each samples in sample train and test split
+plot_train_X, plot_test_X, plot_train_y, plot_test_y = train_test_split(
+    X,
+    y,
+    shuffle=True,
+    random_state=36
+)
+fig2, ax = plt.subplots(1)
+fig2.show()
+
 # Cross Validation
-kf = KFold(n_splits=6, shuffle=True)
-splits = kf.split(X)
-
-model_accuracy = []
-model_precision = []
-model_recall = []
-model_f1score = []
-for split in splits:
-    train_indices, test_indices = split
-    X_train = X[train_indices]
-    X_test = X[test_indices]
-    y_train = y[train_indices]
-    y_test = y[test_indices]
-    model = RandomForestClassifier()
-    model.fit(X_train, y_train)
-    y_pred = model.predict(X_test)
-    y_pred_proba = model.predict_proba(X_test)
-    model_accuracy.append(accuracy_score(y_test, y_pred))
-    model_precision.append(precision_score(y_test, y_pred, average='weighted'))
-    model_recall.append(recall_score(y_test, y_pred, average='weighted'))
-    model_f1score.append(f1_score(y_test, y_pred, average='weighted'))
-
-print('Accuracy: ', np.mean(model_accuracy))
-print('Recall: ', np.mean(model_recall))
-print('Precision: ', np.mean(model_precision))
-print('F1 score: ', np.mean(model_f1score))
+# kf = KFold(n_splits=6, shuffle=True)
+# splits = kf.split(X)
+#
+# model_accuracy = []
+# model_precision = []
+# model_recall = []
+# model_f1score = []
+# for split in splits:
+#     train_indices, test_indices = split
+#     X_train = X[train_indices]
+#     X_test = X[test_indices]
+#     y_train = y[train_indices]
+#     y_test = y[test_indices]
+#     model = RandomForestClassifier()
+#     model.fit(X_train, y_train)
+#     y_pred = model.predict(X_test)
+#     y_pred_proba = model.predict_proba(X_test)
+#     model_accuracy.append(accuracy_score(y_test, y_pred))
+#     model_precision.append(precision_score(y_test, y_pred, average='weighted'))
+#     model_recall.append(recall_score(y_test, y_pred, average='weighted'))
+#     model_f1score.append(f1_score(y_test, y_pred, average='weighted'))
+#
+# print('Accuracy: ', np.mean(model_accuracy))
+# print('Recall: ', np.mean(model_recall))
+# print('Precision: ', np.mean(model_precision))
+# print('F1 score: ', np.mean(model_f1score))
 
 # Plotting results
-# fpr, tpr, threshold = roc_curve(y_test, y_pred_proba)
-# plt.plot()
-
-plt.show()
-
+# fig, ax = plt.subplots()
+# ax.scatter(X_test[:, 0], y_pred_proba[:, 0], label='rock')
+# ax.scatter(X_test[:, 0], y_pred_proba[:, 1], label='paper')
+# ax.scatter(X_test[:, 0], y_pred_proba[:, 2], label='scissors')
+#
+# # plt.subplot()
+# # plt.plot()
+#
+# fig.show()
 
 # Training model
 # X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=35, shuffle=True)
